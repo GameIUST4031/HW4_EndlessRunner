67136831bb4edd72856aaddba8a5e7d74a10ac5d
Mahdioshani <mahdioshani2003@gmail.com>
2024-11-27 15:24:38 +0330

    ADD Horizontal Movement

--- Endless_Runner/Assets/Scripts/PlayerMovement.cs
+++ Endless_Runner/Assets/Scripts/PlayerMovement.cs
@@ -6,11 +6,17 @@ public class PlayerMovement : MonoBehaviour
 {
     // Start is called before the first frame update
     public float PlayerSpeed = 2;
-    
-
-    // Update is called once per frame
+    public float HorizontalSpeed = 3;    // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector3.forward * PlayerSpeed*Time.deltaTime);
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left*HorizontalSpeed*Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right*HorizontalSpeed*Time.deltaTime);
+        }
     }
 }
